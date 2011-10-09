c74441e91bea51d99c0c433bcbc45f7bbb876fc5
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2011-10-09 19:51:15 +0200

    added project
    Signed-off-by: Thomas Sattlecker <thomas.sattlecker@gmail.com>

---
+++ src/com/dslab/genericTaskEngine/GenericTaskEngine.java
@@ -0,0 +1,13 @@
+package com.dslab.genericTaskEngine;
+
+public class GenericTaskEngine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/dslab/scheduler/Scheduler.java
@@ -0,0 +1,13 @@
+package com.dslab.scheduler;
+
+public class Scheduler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
