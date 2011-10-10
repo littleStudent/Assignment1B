ef9cc8778edd8b36378e9f8ff8c104630ffab1d0
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2011-10-10 12:03:42 +0200

    added thread listening on the client
    Signed-off-by: Thomas Sattlecker <thomas.sattlecker@gmail.com>

--- src/com/dslab/client/Client.java
+++ src/com/dslab/client/Client.java
@@ -13,6 +13,12 @@ public class Client {
 	private static int schedulerTCPPort;
 	private static String taskDir;
 	
+	protected Client(String schedulerHost, int schedulerTCPPort, String taskDir) {
+		schedulerHost = this.schedulerHost;
+		schedulerTCPPort = this.schedulerTCPPort;
+		taskDir = this.taskDir;
+	}
+	
 	/**
 	 * @param args
 	 */
@@ -39,26 +45,83 @@ public class Client {
 	         System.exit(1);
 		}
 	}
+
 	
 	private static void runningClient() throws UnknownHostException, IOException {
-		
+		Client callback = new Client(schedulerHost, schedulerTCPPort, taskDir);
+		checkConsoleInput(callback);
 	}
 	
-	private static void checkConsoleInput() throws IOException {
-		InputStreamReader converter = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(converter);
-		while(true) {
-			String input = in.readLine();
+	private static void checkConsoleInput(final Client callback) {
+		new Thread() {
+			public void run() {
+				InputStreamReader converter = new InputStreamReader(System.in);
+				BufferedReader in = new BufferedReader(converter);
+				while(true) {
+					final String input;
+					try {
+						input = in.readLine();
+						System.out.println("ready for input");
+						new Thread() {
+							public void run() {
+								callback.handleInput(input);
+							}
+						}.start();
+												
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+	}
+	
+	private void handleInput(String input) {
+		
+		if(input.lastIndexOf(" ")!=-1 && input.substring(0, 6).equals("!login") && getCharCount(input, " ") == 2) {
+			System.out.println("accepted");
+		} else if(input.substring(0, input.length()).equals("!logout")) {
+			System.out.println("accepted");
+		} else if(input.substring(0, input.length()).equals("!list")) {
+			System.out.println("accepted");
+		} else if(input.lastIndexOf(" ")!=-1 && input.substring(0, 8).equals("!prepare") && getCharCount(input, " ") == 2) {
+			System.out.println("accepted");
+		} else if(input.lastIndexOf(" ")!=-1 && input.substring(0, 14).equals("!requestEngine") && getCharCount(input, " ") == 1) {
+			System.out.println("accepted");
+		} else if(input.lastIndexOf(" ")!=-1 && input.substring(0, 12).equals("!executeTask") && getCharCount(input, " ") == 2) {
+			System.out.println("accepted");
+		} else if(input.lastIndexOf(" ")!=-1 && input.substring(0, 5).equals("!info") && getCharCount(input, " ") == 1) {
+			System.out.println("accepted");
+		} else if(input.substring(0, input.length()).equals("!exit")) {
+			System.out.println("accepted");
+		} else {
+			System.out.println("INCORRECT INPUT");
 		}
 	}
 	
-	private static void registerClient(String registerInput) throws UnknownHostException, IOException {
+	private void registerClient(String registerInput) throws UnknownHostException, IOException {
 		Socket schedulerSocket = new Socket(schedulerHost, schedulerTCPPort);
 		DataOutputStream outToServer = new DataOutputStream(schedulerSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(schedulerSocket.getInputStream()));
 		outToServer.writeBytes(registerInput + "\n");
 		System.out.println("FROM SERVER: " + inFromServer.readLine());
 		schedulerSocket.close();
+	}
+	
+	/**
+	 * count the characters in a specific string
+	 * @param text
+	 * @param ch
+	 * @return
+	 */
+	private int getCharCount(String text, String ch) {
+		int count = 0;
+		while(text.lastIndexOf(ch)!=-1){
+			count++;
+			text = text.substring(text.indexOf(ch) + 1);
+		}
+		return count;
 	}
 
 }
